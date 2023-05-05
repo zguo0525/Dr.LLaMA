6d95f268658a310070cc795a9f5e00f42124f5ab
zg <52820470+zguo0525@users.noreply.github.com>
2023-05-04 20:04:18 -0400

    Update and rename OpenAI.py to openai.py

--- openai.py
+++ openai.py
@@ -26,12 +26,8 @@ for i in range(0, 50):
             max_tokens=512,
             n=1,
             temperature=0.8,
-#             repetition_penalty=2.0,
-#             beam=5,
         )
 
         returned_data.append(response['choices'][0]['message']['content'])
         np.save('gpt3.5_answer_' + str(i) + '.npy', returned_data)
-        #print(idx, returned_data[idx])
         time.sleep(1)
-        #print(response['choices'][0]['message']['content'])
